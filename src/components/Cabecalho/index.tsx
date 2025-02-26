44264edea80b0bc58a11626c81e5eff61301be49
MichelPianaro <teconhecer27@gmail.com>
2025-02-26 15:30:16 -0300

    estilização para o Styled ComponentsCr

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,9 @@
-import styles from './Cabecalho.module.css'
+import { Header, Title } from './Cabecalho.module'
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
-    <h1>EBAC Jobs</h1>
-  </header>
+  <Header>
+    <Title>Michael Jobs</Title>
+  </Header>
 )
 
 export default Cabecalho
